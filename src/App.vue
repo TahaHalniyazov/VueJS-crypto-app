0c0f7870aef4c8cfe3d0f2ee1586e852a68b5126
unknown <halniyazovt98@gmail.com>
2024-02-05 11:13:04 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>CRYPTO</h1>
+    <Input :changeAmount="changeAmount" :convert="convert"/>
+    <p v-if="error != ''">{{ error }}</p>
+    <p v-if="result != ''" className= "result-text">{{ result }}</p>
+    
+
+    <div className= "selectors">
+        <Selector :setCrypto="setCryptoFirst"/>
+        <Selector :setCrypto="setCryptoSecond"/>
+    </div>
+  
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Input from './components/Input.vue'
+import Selector from './components/Selector.vue'
+import CryptoConvert from 'crypto-convert'
 
-nav {
-  padding: 30px;
+const convert = new CryptoConvert()
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default { 
+  components: { Input, Selector },
+  data () {
+    return {
+      amount: 0,
+      cryptoFirst: '',
+      cryptoSecond: '',
+      error: '',
+      result: 0
     }
+  },
+  methods: {
+    changeAmount(val) {
+      this.amount = val
+    },
+    setCryptoFirst(val) {
+      this.cryptoFirst = val
+    },
+    setCryptoSecond(val) {
+      this.cryptoSecond = val
+    },
+    async convert() {
+      if (this.amount <= 0) {
+        this.error = "Введите число больше нуля";
+        return;
+      }
+      else if (this.cryptoFirst == '' || this.cryptoSecond == ''){
+        this.error = "Выберите валюту";
+        return;
+      }
+      else if (this.cryptoFirst == this.cryptoSecond){
+        this.error = "Выберите другую валюту";
+        return;
+      }
+      
+
+      this.error = ''
+
+      await convert.ready()
+
+      if (this.cryptoFirst == 'BTC' && this.cryptoSecond == 'ETH')
+        this.result = convert.BTC.ETH(this.amount);
+      else if (this.cryptoFirst == 'BTC' && this.cryptoSecond == 'USDT')
+        this.result = convert.BTC.USDT(this.amount);
+      else if (this.cryptoFirst == 'ETH' && this.cryptoSecond == 'BTC')
+        this.result = convert.ETH.BTC(this.amount);
+      else if (this.cryptoFirst == 'ETH' && this.cryptoSecond == 'USDT')
+        this.result = convert.ETH.USDT(this.amount);
+      else if (this.cryptoFirst == 'USDT' && this.cryptoSecond == 'BTC')
+        this.result = convert.USDT.BTC(this.amount);
+      else if (this.cryptoFirst == 'USDT' && this.cryptoSecond == 'ETH')
+        this.result = convert.USDT.ETH(this.amount);
+      
+
+    }
+    
   }
+
+    
 }
+</script>
+
+<style>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
+}
+
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Введите число"><br/>
+        <button @click="convert()">Конвертировать</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            changeAmount: {
+                type: Function,
+                required: true
+            },
+            convert: {
+                type: Function,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+input { 
+    width: 500px;
+    outline: none; 
+    border-radius: 3px;
+    position: relative;
+    top: -50px;
+    font-size: 1.25em;
+    border: 0;
+    padding: 15px 20px;
+    background: #fafafa;
+    color: #333 ;
+}
+button {
+    position: relative;
+    top: -20px;   
+    padding: 15px 20px;
+    color: white;
+    text-transform: uppercase;
+    cursor: pointer;
+    background: #1A032D;
+    border: 0;
+    border-radius: 3px;
+
+}
+</style>
